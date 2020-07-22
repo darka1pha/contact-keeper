37049f1bda0873d15516a8fdaa5615dd2afdd05d
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2020-07-22 20:14:57 +0430

    writing 2 test

--- test/api/app.test.js
+++ test/api/app.test.js
@@ -11,3 +11,12 @@ test('Test For Register a New User' , async () => {
     })
     .expect(200)
 })
+
+test('Test For Login a New User' , async () => {
+    await request(app).post('/api/auth')
+    .send({
+        email:'test@gmail.com',
+        password:'123456'
+    })
+    .expect(200)
+})
